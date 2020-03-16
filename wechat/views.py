b75ba354eb24255564a561d41addf36e481d4df0
xuxiaolin@git.minrow.com <xuxiaolin>
2020-03-16 16:29:12 +0800

    t

--- wechat/views.py
+++ wechat/views.py
@@ -46,34 +46,35 @@ class Wechat(View):
             return JsonResponse({"msg": "fail"})
 
     def post(self, request):
-        sToken = "hQybAUE112YajhcxCQ"
-        sEncodingAESKey = "ytr9W97MuSuIDuDXbnwpBLjejBv47kycLRKCLCvWOMy"
-        sCorpID = "ww12846bc6b96876c4"
+        try:
+            signature = request.POS.get("signature")  # 先获取加密签名
+            timestamp = request.POS.get("timestamp")  # 获取时间戳
+            nonce = request.POS.get("nonce")  # 获取随机数
+            echostr = request.POS.get("echostr")  # 获取随机字符串
+            token = "sldmlsmlm"  # 自己设置的token
+            # 使用字典序排序（按照字母或数字的大小顺序进行排序）
+            print(signature)
+            list = [token, timestamp, nonce]
+            list.sort()
 
-        wxcpt = WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID)
+            # 进行sha1加密
+            temp = ''.join(list)
+            sha1 = hashlib.sha1(temp.encode('utf-8'))
+            hashcode = sha1.hexdigest()
 
-        # sReqMsgSig = HttpUtils.ParseUrl("msg_signature")
-        sReqMsgSig = request.GET.get("msg_signature")
-        sReqTimeStamp = request.GET.get("timestamp")
-        sReqNonce = request.GET.get("nonce")
-        sReqData = request.body
-        sReqData = sReqData.decode('gbk')
-        print(sReqData)
-        ret, sMsg = wxcpt.DecryptMsg(sReqData, sReqMsgSig, sReqTimeStamp, sReqNonce)
-        print(ret, sMsg)
-        if (ret != 0):
-            print("ERR: DecryptMsg ret: " + str(ret))
-            return HttpResponse({"msg": "fail"})
-        # 解密成功，sMsg即为xml格式的明文
-        else:
-            sMsg = sMsg.decode('utf8')
-            # json_str = xmltodict.parse(sMsg)
-            # print(json_str)
-            # xml_tree = ET.fromstring(sMsg)
-            # print(xml_tree)
-            # content = xml_tree.find("Content").text
-            # print(content)
-            return HttpResponse("sucess!")
+            # 将加密后的字符串和signatrue对比，如果相同返回echostr,表示验证成功
+            if hashcode == signature:
+                print("成功")
+                if echostr:
+                    return JsonResponse(int(echostr), safe=False)
+                else:
+                    return JsonResponse({"msg": "fail"})
+            else:
+                return JsonResponse({"msg": "fail"})
+
+        except Exception as e:
+            print(e)
+            return JsonResponse({"msg": "fail"})
 
 
 class Tutorial(View):

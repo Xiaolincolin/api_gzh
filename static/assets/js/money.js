d1d658dba11c5d8ad8a81b9dbc48463a318134d6
小林 <1144759420@qq.com>
2020-03-19 11:21:55 +0800

    修改提现页面

---
+++ static/assets/js/money.js
@@ -0,0 +1,27 @@
+$("#money").focus(function(){
+	$("#money").css("background-color","#FFFFCC");
+});
+$("#money").blur(function(){
+	$("#money").css("background-color","#D6D6FF");
+});
+
+$("#name").focus(function(){
+	$("#name").css("background-color","#FFFFCC");
+});
+$("#name").blur(function(){
+	$("#name").css("background-color","#D6D6FF");
+});
+
+$("#account").focus(function(){
+	$("#account").css("background-color","#FFFFCC");
+});
+$("#account").blur(function(){
+	$("#account").css("background-color","#D6D6FF");
+});
+
+$("#remarks").focus(function(){
+	$("#remarks").css("background-color","#FFFFCC");
+});
+$("#remarks").blur(function(){
+	$("#remarks").css("background-color","#D6D6FF");
+});

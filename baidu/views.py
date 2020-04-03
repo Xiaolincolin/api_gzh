85822fd8952e5b4fb41959da4092611cc43a3e0f
小林 <1144759420@qq.com>
2020-04-03 12:44:23 +0800

    log

--- baidu/views.py
+++ baidu/views.py
@@ -1,4 +1,6 @@
 import json
+import logging
+from datetime import datetime
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -19,8 +21,19 @@ class Baidu(View):
         return HttpResponse("错误的请求")
 
     def post(self, request):
+        logger_money = logging.getLogger('baidu')
         data = request.POST.get("data")
         if data:
+            tmp_data = json.loads(data)
+            product = tmp_data.get("product", "")
+            creative = tmp_data.get("creative", "")
             ktw.sendjsondata(data)
-            print('百度发送成功')
+            msg = str(self.get_now_str()) + " " + product + " " + str(creative) + " " + "发送成功"
+            logger_money.info(msg)
+        else:
+            msg = str(self.get_now_str()) + " data为空"
+            logger_money.info(msg)
         return HttpResponse("成功")
+
+    def get_now_str(self):
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')

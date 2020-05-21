b76cf81fe996d72e0a6d8a4efbff2fa4a982b3c8
小林 <1144759420@qq.com>
2020-05-21 17:24:44 +0800

    v2

--- pyq/views.py
+++ pyq/views.py
@@ -9,7 +9,7 @@ from baidu.utils.utils import KafkaTopicWriter
 max_records = 1000
 max_elapsed_time = 10
 bootstrap_servers_list = ["node1:19092", "node2:19092", "node3:19092"]
-ktw = KafkaTopicWriter(bootstrap_servers_list, "adi_flow_pyq_v1", acks='all')
+ktw = KafkaTopicWriter(bootstrap_servers_list, "adi_flow_pyq_v2", acks='all')
 
 # Create your views here.
 
